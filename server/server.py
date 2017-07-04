dd454d32201dbc8a0db0d49dc8105c4f76237bda
User <paadams@nmu.edu>
2017-07-04 13:04:37 -0400

    created proxy to python server

--- server/server.py
+++ server/server.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import nltk
+sentence = "Hello from Python server"
+sen_tok = nltk.word_tokenize(sentence)
 
 app = Flask(__name__)
 api = Api(app)
 
 class Test(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'greetings': sen_tok}
 
-api.add_resource(Test, '/')
+api.add_resource(Test, '/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
