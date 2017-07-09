60b1b0448c07795c814cacd35d591d7484e40e03
User <paadams@nmu.edu>
2017-07-09 14:06:33 -0400

    added route

--- client/parsnips/src/components/FreqDist.vue
+++ client/parsnips/src/components/FreqDist.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="freq-dist">
-
+    <ul>
+      <li v-for="(data, key) in dataFromServer" :key="key">
+        {{data}}
+      </li>
+    </ul>
+    <router-link to="/">return home</router-link>
   </div>
 </template>
+<script>
+export default {
+  name: 'freq-dist',
+  created () {
+    fetch('/api').then(response => {
+      return response.json()
+    }).then(data => {
+      console.log(data)
+      this.loadData(data)
+    })
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      dataFromServer: []
+    }
+  },
+  methods: {
+    loadData (data) {
+      this.dataFromServer = data
+    }
+  }
+}
+</script>
+
---
+++ client/parsnips/src/components/MainPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main-page">
+        <h1>Main Page</h1>
+        <router-link to="/freqdist">View Frequency Distribution</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'main-page'
+}
+</script>
+
