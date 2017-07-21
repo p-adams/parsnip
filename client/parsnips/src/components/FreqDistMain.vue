1179876e38d8563c2014a6d97a316f1409373fe0
User <paadams@nmu.edu>
2017-07-21 07:40:23 -0400

    updated FreqDist form

--- client/parsnips/src/components/FreqDistMain.vue
+++ client/parsnips/src/components/FreqDistMain.vue
@@ -6,7 +6,10 @@
         {{data}}
       </li>
     </ul>
-      <form :style="{border: '1px solid red'}" v-if="!textSubmitted">
+      <form
+          @submit="sendText"
+          :style="{border: '1px solid red'}"
+          v-if="!textSubmitted">
         <textarea
             v-model="text"
             rows="10"
@@ -16,7 +19,7 @@
         >
         </textarea>
         <br>
-        <button @click="sendText">send text</button>
+        <button type="submit">send text</button>
       </form>
       <freq-dist v-else="textSubmitted"></freq-dist>
     <router-link to="/">return home</router-link>
@@ -44,6 +47,13 @@ export default {
       this.dataFromServer.push(...data.greetings)
     },
     sendText () {
+      console.log(this.text)
+      fetch('/api', {
+        method: 'post',
+        body: {text: this.text, op: 'freq-dist'}
+        })
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
       this.textSubmitted = true
     }
   },
--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 import nltk
 sentence = "Hello from Python server"
 sen_tok = nltk.word_tokenize(sentence)
@@ -7,11 +7,18 @@ sen_tok = nltk.word_tokenize(sentence)
 app = Flask(__name__)
 api = Api(app)
 
-class Test(Resource):
+parser = reqparse.RequestParser()
+parser.add_argument('nlp_data')
+
+class NLProcessor(Resource):
     def get(self):
         return {'greetings': sen_tok}
+    
+    def post(self):
+        args = parser.parse_args()
+        print(args['nlp_data'])
 
-api.add_resource(Test, '/api')
+api.add_resource(NLProcessor, '/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
