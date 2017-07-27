ecc3f540f1077a1539f77df0db8b8d52978e5069
User <paadams@nmu.edu>
2017-07-26 22:47:18 -0400

    added toolbar component

--- client/parsnips/src/components/MainPage.vue
+++ client/parsnips/src/components/MainPage.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="main-page">
-        <h3>parsnips</h3>
-        <router-link to="/freqdist">View Frequency Distribution</router-link>
+      <v-toolbar
+        <v-toolbar-title>parsnips</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <router-link to="/freqdist">Word Frequency Distribution</router-link>
+          <router-link to="/ner">Named-Entity Recognition</router-link>
+          <router-link to="/tok">Tokenization</router-link>
+          <router-link to="/parse">Parsing</router-link>
+          <router-link to="/pos">POS Tagging</router-link>
+          <router-link to="/lemma">Lemmatization</router-link>
+          <router-link to="/cr">Coreference Resolution</router-link>
+        </v-toolbar-items>
+      </v-toolbar>
   </div>
 </template>
 <script>
