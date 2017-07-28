3915b442f515a9f0c0dfacf5c5627d6b4e3d60bc
User <paadams@nmu.edu>
2017-07-28 15:44:49 -0400

    updated Navigation component

--- client/parsnips/src/components/MainPage.vue
+++ client/parsnips/src/components/MainPage.vue
@@ -1,14 +1,28 @@
 <template>
   <v-app>
-    <toolbar></toolbar>
+    {{isOpen}}
+    <navigation :open="isOpen"></navigation>
+    <toolbar :open="onToggleOpen"></toolbar>
   </v-app>
 </template>
 <script>
 import Toolbar from './Toolbar'
+import Navigation from './Navigation'
 export default {
   name: 'main-page',
+  data () {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    onToggleOpen () {
+      this.isOpen = !this.isOpen
+    }
+  },
   components: {
-    Toolbar
+    Toolbar,
+    Navigation
   }
 }
 </script>
--- client/parsnips/src/components/Navigation.vue
+++ client/parsnips/src/components/Navigation.vue
@@ -1,11 +1,50 @@
 <template>
+  <div>
+      meow {{isOpen}}
     <v-navigation-drawer
         class="pb-0"
         persistent
         absolute
         height="100%"
         clipped
-        enabl-resize-watcher
-        v-model="drawer">
+        enable-resize-watcher
+        v-model="isOpen"
+    >
+        <v-list dense>
+            <v-list-tile v-for="(route, key) in routes" :key=key>
+                    <v-list-tile-content>
+                        <v-list-tile-title>
+                            {{route.text}}
+                        </v-list-tile-title>
+                    </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
     </v-navigation-drawer>
+    </div>
 </template>
+<script>
+export default {
+  name: 'navigation',
+  props: ['open'],
+  updated () {
+    console.log(this.open)
+  },
+  data () {
+      return {
+        isOpen: this.open,
+        routes: [
+            {link: "/freqdist", text: "Word Frequency Distribution"}
+        ]
+      }
+  }
+}
+</script>
+ <!--<v-toolbar-items class="hidden-sm-and-down">
+        <router-link to="/freqdist">Word Frequency Distribution</router-link>
+        <router-link to="/ner">Named-Entity Recognition</router-link>
+        <router-link to="/tok">Tokenization</router-link>
+        <router-link to="/parse">Parsing</router-link>
+        <router-link to="/pos">POS Tagging</router-link>
+        <router-link to="/lemma">Lemmatization</router-link>
+        <router-link to="/cr">Coreference Resolution</router-link>
+    </v-toolbar-items>-->
