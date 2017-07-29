2ebee6a492f46e151160881bc29dd4c34dcb9867
User <paadams@nmu.edu>
2017-07-29 11:40:27 -0400

    updated Routes

--- client/parsnips/src/App.vue
+++ client/parsnips/src/App.vue
@@ -1,12 +1,63 @@
 <template>
   <div id="app">
+    <md-toolbar md-theme="green">
+      <md-button class="md-icon-button" @click="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <h2 class="md-title">parsnips</h2>
+      <span style="flex: 1;"></span>
+      <md-button class="md-icon-button home" @click="home">
+        <md-icon>home</md-icon>
+      </md-button>
+    </md-toolbar>
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Supported NLP Features</h3>
+        </div>
+      </md-toolbar>
+    <md-list>
+      <md-list-item v-for="(route, key) in routes" :key=key>
+        <router-link :to="route.link">{{route.text}}</router-link>
+      </md-list-item>
+    </md-list>
+    </md-sidenav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      isOpen: false,
+      routes: [
+            {link: "/tokenization", text: "Tokenization"},
+            {link: "/freqdist", text: "Word Frequency Distribution"},
+            {link: "/ner", text: "Named-Entity Recognition"},
+            {link: "/lemma", text: "Lemmatization"},
+            {link: "/pos", text: "Part of Speech Tagging"},
+            {link: "/con-parse", text: "Constituency Parsing"},
+            {link: "/dep-parse", text: "Dependency Parsing"},
+            {link: "/coref-res", text: "Coreference Resolution"},
+      ]
+    }
+  },
+  methods: {
+    home () {
+      this.$router.push({name: 'Home'})
+    },
+    toggleLeftSidenav() {
+      this.$refs.leftSidenav.toggle();
+    },
+    open(ref) {
+      console.log('Opened: ' + ref);
+    },
+    close(ref) {
+      console.log('Closed: ' + ref);
+    }
+  }
 }
 </script>
 
--- client/parsnips/src/components/MainPage.vue
+++ client/parsnips/src/components/MainPage.vue
@@ -1,22 +1,6 @@
 <template>
   <div>
-    <md-toolbar>
-    <md-button class="md-icon-button" @click="toggleLeftSidenav">
-      <md-icon>menu</md-icon>
-    </md-button>
-
-    <h2 class="md-title">My App</h2>
-  </md-toolbar>
-
-  <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
-    <md-toolbar class="md-large">
-      <div class="md-toolbar-container">
-        <h3 class="md-title">Sidenav content</h3>
-      </div>
-    </md-toolbar>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi cupiditate esse necessitatibus beatae nobis, deserunt ut est fugit, tempora deleniti, eligendi commodi doloribus. Nemo, assumenda possimus, impedit inventore perferendis iusto!</p>
-  </md-sidenav>
+    Main Page
   </div>
 </template>
 <script>
@@ -26,7 +10,6 @@ export default {
     return {
       isOpen: false,
       routes: [
-            {link: "/", text: "Home"},
             {link: "/tokenization", text: "Tokenization"},
             {link: "/freqdist", text: "Word Frequency Distribution"},
             {link: "/ner", text: "Named-Entity Recognition"},
@@ -52,4 +35,8 @@ export default {
 }
 </script>
 <style>
+  .md-icon-button.home {
+    border: 1px solid white;
+    
+  }
 </style>
