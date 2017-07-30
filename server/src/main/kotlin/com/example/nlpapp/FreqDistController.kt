a04636d46553308672c90e173037b005926dc16f
User <paadams@nmu.edu>
2017-07-30 18:59:17 -0400

    added tokenization controller

--- server/src/main/kotlin/com/example/nlpapp/FreqDistController.kt
+++ server/src/main/kotlin/com/example/nlpapp/FreqDistController.kt
@@ -4,9 +4,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import edu.stanford.nlp.simple.*
 import org.springframework.web.bind.annotation.*
-import java.util.HashMap
-
-
 
 class FreqDist() {
     var data: String = ""
---
+++ server/src/main/kotlin/com/example/nlpapp/TokenizationController.kt
@@ -0,0 +1,25 @@
+package com.example.nlpapp
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import edu.stanford.nlp.simple.*
+import org.springframework.web.bind.annotation.*
+
+class TokData {
+    var data = ""
+}
+
+@RestController
+class TokenizationController {
+
+    @RequestMapping("/api/tokenize")
+    fun fetchDemoData(): List<String> {
+        return Sentence("Colorless green ideas sleep furiously").words()
+    }
+
+    @RequestMapping(value = "/api/tokenize", method = arrayOf(RequestMethod.POST))
+    fun getTokenizedText(@RequestBody tokData: TokData): ResponseEntity<List<String>>{
+        val tokenized = mutableListOf<String>()
+        return ResponseEntity <List<String>>(tokenized, HttpStatus.OK)
+    }
+}
