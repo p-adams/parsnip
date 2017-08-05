3b0f3f4a2c0e4b5aa010b9d9ad8afd6d2a7ad266
User <paadams@nmu.edu>
2017-08-04 22:07:18 -0400

    updated NERController

--- server/src/main/kotlin/com/example/nlpapp/NERController.kt
+++ server/src/main/kotlin/com/example/nlpapp/NERController.kt
@@ -1,2 +1,26 @@
 package com.example.nlpapp
 
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import edu.stanford.nlp.simple.*
+import org.springframework.web.bind.annotation.*
+
+class NERData {
+    var data = ""
+}
+
+@RestController
+class NERController {
+    fun createLabels(ndata: NERData): MutableMap<String, String> {
+        var res = mutableMapOf<String, String>()
+        
+        return res
+    }
+    @RequestMapping("/api/ner")
+    fun fetchDemo(): MutableMap<String, String> {
+        var nerd = NERData()
+        nerd.data = "John from Boston worked tirelessly as a mechanic"
+        return createLabels(nerd)
+    }
+}
