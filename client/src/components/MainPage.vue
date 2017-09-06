27f93098a9e12756d0f9bfc19d78f373dcd68973
User <paadams@nmu.edu>
2017-09-05 23:04:34 -0400

    attempted to fix parser

--- client/src/components/MainPage.vue
+++ client/src/components/MainPage.vue
@@ -3,14 +3,20 @@
       <md-layout :style="{marginTop: '25px'}" md-flex="25">
       <h2>Parsnips</h2>
       <h4>Parse and analyze text snippets using CoreNLP</h4>
+      {{res}}
     </md-layout>
   </md-layout>
 </template>
 <script>
+import axios from 'axios'
 export default {
   name: 'main-page',
+  created () {
+    this.loadParsedData()
+  },
   data () {
     return {
+      res: '',
       isOpen: false,
       routes: [
             {link: "/tokenization", text: "Tokenization"},
@@ -33,6 +39,37 @@ export default {
     },
     close(ref) {
       console.log('Closed: ' + ref);
+    },
+    balancedParens (str) {
+      var stack = [];
+      var open = { '{': '}', '[': ']', '(': ')' };
+      var closed = { '}': true, ']': true, ')': true };
+      
+      for (var i = 0; i < str.length; i ++) {
+        var chr = str[i];
+        if (open[chr]) {
+          stack.push(chr);
+        } else if (closed[chr]) {
+          if (open[stack.pop()] !== chr) return false;
+        }
+      }
+      return stack.length === 0;
+    },
+    parseData (data) {
+      let str = data.substring(data.indexOf('S'), data.length - 2)
+      console.log(str)
+      let idx = str.indexOf('(')
+      console.log(idx)
+      console.log(this.balancedParens(str.slice(idx + 1)))
+    },
+    loadParsedData () {
+      axios.get('api')
+        .then(res => {
+          this.parseData(res.data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
     }
   }
 }
--- server/src/main/kotlin/com/example/nlpapp/ConstParseController.kt
+++ server/src/main/kotlin/com/example/nlpapp/ConstParseController.kt
@@ -4,3 +4,13 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import edu.stanford.nlp.simple.*
 import org.springframework.web.bind.annotation.*
+
+@RestController
+class ConstParseController {
+    @RequestMapping("/api")
+    fun fetchDemoTags(): String {
+        val sent = Sentence("colorless green ideas sleep furiously")
+        println(sent.parse())
+        return sent.parse().toString()
+    }
+}
